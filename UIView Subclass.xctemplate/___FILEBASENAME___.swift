b45d148f73f81ebb01b393fb8d2574437147300d
iliaskarim <ilias@atom.finance>
2021-09-23 18:56:14 -0400

    Update ___FILEBASENAME___.swift

--- UIView Subclass.xctemplate/___FILEBASENAME___.swift
+++ UIView Subclass.xctemplate/___FILEBASENAME___.swift
@@ -15,11 +15,16 @@ final class ___FILEBASENAME___: UIView {
   private weak var delegate: ___FILEBASENAME___Delegate?
   private let viewModel: ___FILEBASENAME___Model
   
+  private func addSubviews() {
+    <#code#>
+  }
+  
   /// Designated initializer
   init(delegate: ___FILEBASENAME___Delegate, viewModel: ___FILEBASENAME___Model) {
     self.delegate = delegate
     self.viewModel = viewModel
     super.init(frame: .zero)
+    addSubviews()
   }
 
   required init?(coder: NSCoder) {
